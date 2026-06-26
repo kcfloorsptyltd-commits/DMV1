c89326eeaa1ec497608955b002d7d232ee646808
kcfloorsptyltd-commits <kcfloorsptyltd@gmail.com>
2026-06-26 16:23:20 +1000

    fix: add RSN button handlers to ticket.js interaction file

--- src/interactions/buttons/ticket.js
+++ src/interactions/buttons/ticket.js
@@ -6,6 +6,8 @@ import createTicketHandler, {
   unclaimTicketHandler,
   reopenTicketHandler,
   deleteTicketHandler,
+  linkRsnButtonHandler,
+  unlinkRsnButtonHandler,
 } from '../../handlers/ticketButtons.js';
 
 export default [
@@ -17,4 +19,6 @@ export default [
   unclaimTicketHandler,
   reopenTicketHandler,
   deleteTicketHandler,
+  linkRsnButtonHandler,
+  unlinkRsnButtonHandler,
 ];
